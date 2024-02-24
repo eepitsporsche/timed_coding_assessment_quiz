0184fc48d6eb3297b53445b9771e7a3944fe3272
Porsche <herskorn@gmail.com>
2024-02-24 11:04:52 -0800

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+//button styles when clicked
+
+//function for timer counter
+
+//function to produce user's score
+
+//log user's initials
+
+//function to calculate and display high scores
+
+//function to clear high scores
