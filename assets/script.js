30e784a6af8fee81798fe23760648e29d56e7504
Porsche <herskorn@gmail.com>
2024-02-24 12:42:22 -0800

    "added timer function"

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,31 @@
 //button styles when clicked
 
 //function for timer counter
+var timer = document.getElementById("timer");
+
+function countdown() {
+    var secondsLeft = 75;
+
+    timer.textContent = "Time: " + secondsLeft;
+
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+
+        timer.textContent = "Time: " + secondsLeft;
+    
+     if (secondsLeft <= 0) {
+        clearInterval(timerInterval);
+    }
+    }, 1000);
+}
+
+countdown();
+
+
+//function to calculate and display high scores
 
 //function to produce user's score
 
 //log user's initials
-
-//function to calculate and display high scores
 
 //function to clear high scores
